9c1b5e141e4e24bc547e0ba55d711c904fc6bedd
Jim Wheeler <jwheele1@gmail.com>
2024-06-07 09:37:11 -0500

    Revised config file

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -70,22 +70,19 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: "Home",
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: "My Site Logo",
+          src: "img/docusaurus.png",
         },
         items: [
           {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Tutorial',
+            "label": "Documentation",
+            "to": "docs"
           },
-          { to: '/blog', label: 'Blog', position: 'left' },
           {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
+            href: 'https://irmi.zendesk.com/hc/en-us',
+            label: 'Zendesk',
             position: 'right',
           },
         ],
