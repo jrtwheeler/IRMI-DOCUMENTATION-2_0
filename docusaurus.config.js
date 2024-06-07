f9c73be9c8687b34cb70f65a7d77efc58175094a
Jim Wheeler <jwheele1@gmail.com>
2024-06-07 14:34:12 -0500

    Config changes to lines 3 - 58

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,23 +8,20 @@ const { themes: prismThemes } = require('prism-react-renderer');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
-
-  // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  title: "IRMI Learning",
+  tagline: "Learning center for internal processes",
+  url: 'https://your-docusaurus-test-site.com',
+  customFields: {
+    // Put your custom environment here
+    first_key: "JimVALUEONE",
+    second_key: "JimVALUETWO",
+  },
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/docusaurus.png",
+  organizationName: "IRMI", // Usually your GitHub org/user name.
+  projectName: "docusaurus", // Usually your repo name.
 
   themes: [
     // ... Your other themes.
@@ -46,16 +43,7 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // editUrl: 'https://https://github.com/jrtwheeler/IRMI_Doc_Site',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -68,7 +56,6 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
       navbar: {
         title: "Home",
         logo: {
